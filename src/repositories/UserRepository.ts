e307a20c7bacba97c4f1f10dbcfddf26137e477a
nicholasscabral <nicholasscabral@gmail.com>
2021-10-12 16:41:27 -0300

    find by id method

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -2,4 +2,8 @@ import { EntityRepository, Repository } from "typeorm";
 import { User } from "../models/User";
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async findById(id: string): Promise<User> {
+    return this.findOne(id);
+  }
+}
