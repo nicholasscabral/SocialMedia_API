3419b4d22268df489c78f0a346495d5ff385db2e
nicholasscabral <nicholasscabral@gmail.com>
2021-10-27 20:02:47 -0300

    updating table creation (migration)

--- src/database/migrations/1635270824043-CreatePost.ts
+++ src/database/migrations/1635270824043-CreatePost.ts
@@ -4,15 +4,14 @@ export class CreatePost1635270824043 implements MigrationInterface {
   name = "CreatePost1635270824043";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `CREATE TABLE "posts" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(),
-       "user_id" uuid NOT NULL,
-       "desc" character varying NOT NULL,
-       "img" character varying NOT NULL,
-       "likes" text NOT NULL,
-       "created_at" TIMESTAMP NOT NULL DEFAULT now(),
-       CONSTRAINT "PK_2829ac61eff60fcec60d7274b9e" PRIMARY KEY ("id"))`
-    );
+    await queryRunner.query(`CREATE TABLE "posts" (
+    "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+    "user_id" uuid,
+    "desc" character varying NOT NULL,
+    "img" character varying NOT NULL,
+    "likes" text NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT now(),
+    CONSTRAINT "PK_2829ac61eff60fcec60d7274b9e" PRIMARY KEY ("id"))`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
