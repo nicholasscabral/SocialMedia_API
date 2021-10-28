e87f4e222d5b2e911559554c02e6f85d22fb549f
nicholasscabral <nicholasscabral@gmail.com>
2021-10-27 22:47:24 -0300

    sync false

--- src/database/index.ts
+++ src/database/index.ts
@@ -9,7 +9,7 @@ export default async (): Promise<Connection> => {
         process.env.NODE_ENV === "test"
           ? "social_media_api_test"
           : defaultOptions.database,
-      synchronize: process.env.NODE_ENV === "test" ? false : true,
+      // synchronize: process.env.NODE_ENV === "test" ? false : true,
     })
   );
 };
